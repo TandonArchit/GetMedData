cf47bfe9991aa08c13f6280cc45995596ee39339
Archit Tandon <89405762+TandonArchit@users.noreply.github.com>
2022-02-05 20:26:25 -0500

    v3

--- pdfManage.py
+++ pdfManage.py
@@ -1,16 +1,27 @@
 ## The following code is to generate a combined output file as a PDF.
 
-## pip install reportlab
 
+# First, the sample data is defined.
+
+# Name
 nameVar = 'Name: ' + 'John Doe'
 
+# Date of Birth
 dob = 'DOB: ' + '01/01/01'
 
+# Gene with the mutation
 gm  = 'Gene: ' + 'ARSE'
 
+# Extra information given
 smry = 'Info: ' + ' -- SAMPLE INFO -- -- SAMPLE INFO -- -- SAMPLE INFO -- '
 
+
+## Now, we can make the output PDF File
+
 from reportlab.pdfgen.canvas import Canvas
+
+
+## All the data is put on the pdf with 100 point gaps in between, to space out the data.
 
 canvas = Canvas("output.pdf")
 
@@ -21,5 +32,8 @@ canvas.drawString(72, 670, dob)
 canvas.drawString(72, 570, gm)
 
 canvas.drawString(72, 470, smry)
+
+
+# PDF is then saved.
 
 canvas.save()
